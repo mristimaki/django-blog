876ad31670bc58cf7e9e51e628dcd59a0c97665a
mristimaki <rosanna.ristimaki@gmail.com>
2023-07-22 00:12:32 +0000

    Add comment form

--- codestar/settings.py
+++ codestar/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'cloudinary',
     'django_summernote',
+    'crispy_forms',
     'blog',
 ]
 
@@ -58,6 +59,8 @@ LOGOUT_REDIRECT_URL = '/'
 
 # Prevent 500 errors during login and registration
 ACCOUNT_EMAIL_VERIFICATION = 'none'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
